514f8e3f8c5fb242b78549d3e11a0f12a6d05953
unknown <cmine726@gmail.com>
2020-07-30 21:36:56 +0200

    rizando el rizo

--- js/random.js
+++ js/random.js
@@ -1,12 +1,13 @@
+function uwu(){
 var frase = ["Puro entretenimiento :)","Dale en <a href=\"https://github.com/tonicraft18\" style=\"text-decoration: none\">mi GitHub :D"+
-"</a>","Junior Programmer","S&iacute;gueme tambi&eacute;n en twitter uwu","Buena m&uacute;sica&nbsp;¿no bro?"]
-var num = Math.random() * (5+1) - 1
-var rand = document.getElementById('rand')
+"</a>","Junior Programmer","S&iacute;gueme tambi&eacute;n en twitter uwu","Buena m&uacute;sica&nbsp;&iquest;no bro?"]
+var num = Math.random() * (5+1) - 1;
 num = parseInt(num)
 for (var i = 0; i < frase.length; i++) {
   if(num == i){
-    rand.innerHTML = "random";
+    document.getElementById("rand").innerHTML = frase[i];
     break;
   }
 }
 console.log(num)
+}
