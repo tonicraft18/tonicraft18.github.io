ed041b077f8755729455f090f4420f28fcc2732b
unknown <cmine726@gmail.com>
2020-08-01 20:12:16 +0200

    mas frases inteligentes

--- js/random.js
+++ js/random.js
@@ -1,7 +1,9 @@
 function uwu(){
 var frase = ["Puro entretenimiento :)","Dale en <a href=\"https://github.com/tonicraft18\" style=\"text-decoration: none\">mi GitHub :D"+
-"</a>","Junior Programmer","S&iacute;gueme tambi&eacute;n en twitter uwu","Buena m&uacute;sica&nbsp;&iquest;no bro?"]
-var num = Math.random() * (5+1) - 1;
+"</a>","Junior Programmer","S&iacute;gueme tambi&eacute;n en twitter uwu","Buena m&uacute;sica&nbsp;&iquest;no bro?","Web Deevelopeeer",
+"Música: Casin - glue70","Ingeniero en camino :D","Periodic Hour?","uwu","Energy = Much * Coffe^2","idk what put here","git commit -m"+
+"\"añadiendo bugs y novedades :P\"","Pulsa algún botón de aquí abajo :P","Frases random, no está mal eh?","¿Copia de Minecraft?, sip"]
+var num = Math.random() * (frase.length+1) - 1;
 num = parseInt(num)
 for (var i = 0; i < frase.length; i++) {
   if(num == i){
